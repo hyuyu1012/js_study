d48fc55642aaadad162c3795e92796eefdf0c153
hyuyu1012 <hanhyeongu495@gmail.com>
2025-05-28 10:15:34 +0900

    5/28

--- 백준 단계별로 풀어보기/4) 1차원 배열/10811.js
+++ 백준 단계별로 풀어보기/4) 1차원 배열/10811.js
@@ -2,12 +2,23 @@ const fs = require('fs');
 const init = fs.readFileSync("example.txt").toString().trim().split("\n");
 const [N,M] = init[0].split(" ").map(Number);
 
+
+// 배열 초기화
 let arr = new Array(N).fill().map((arr, i) => {
   return i + 1;
 });
 
+// swap
 for (let a = 1; a <=M; a++) {
-  const [i,j] = init[a].split(" ").map(Number);
-  const temp = j - i;
-  
+  let [i,j] = init[a].split(" ").map(Number);
+  while(true) {
+    if (i===j || i>j) {
+      break;
+    }
+    [arr[i-1] , arr[j-1]] = [arr[j-1], arr[i-1]];
+    i++;
+    j--;
+  }
 }
+
+console.log(arr.join(" "));
