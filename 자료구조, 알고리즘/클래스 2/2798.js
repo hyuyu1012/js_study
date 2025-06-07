bcaecb79d864123749ea2b2868e0c8d0bb12057c
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-07 12:18:14 +0900

    6/7

--- 자료구조, 알고리즘/클래스 2/2798.js
+++ 자료구조, 알고리즘/클래스 2/2798.js
@@ -4,3 +4,15 @@ const init = fs.readFileSync("example.txt").toString().split("\n");
 const [N,M] = init[0].split(" ").map(Number);
 const numbers = init[1].split(" ").map(Number);
 
+let max = 0;
+for (let i = 0; i<=N-1; i++) {
+  for (let j = i+1; j <= N-1; j++){
+    for (let k = j+1; k <= N-1; k++) {
+      const result = numbers[i] + numbers[j] + numbers[k];
+      if (result > max && result <= M){
+          max = result;
+      }
+    }
+  }
+}
+console.log(max);
