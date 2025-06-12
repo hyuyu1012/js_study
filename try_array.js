f481ce57f990bcec752dd05e7918c47e46e46c5c
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-12 15:05:46 +0900

    6/12

--- try_array.js
+++ try_array.js
@@ -1,36 +1,4 @@
-//배열의 본질은 객체
-// 순서가 있는 자료구조, 요소들의 나열, 길이, 들어가는 요소를 가변적으로 정할 수 있다.
+arr = [1,2,3,4,5];
 
-const arr = [1,2,3];
+arr.push(arr.shift());
 console.log(arr);
-
-const arr2 = Array();
-console.log(arr2);
-
-
-// undefined 3개
-const arr3 = Array(3);
-console.log(arr3);
-console.log(arr3[0]);
-
-
-const arr4 = Array('4');
-console.log(arr4);
-
-const arr5 = new Array(1);
-console.log(arr5);
-
-const arr6 = new Array('1');
-console.log(arr6);
-
-
-const arr7 = new Array(2).fill(0);
-console.log(arr7);
-
-const arr8 = new Array('2').fill(1);
-console.log(arr8);
-
-const arr9 = new Array(14).fill(0).map((v,i) => i+1);
-console.log(arr9);
-
-// 이차원 배열
--- 자료구조, 알고리즘/클래스 2/1966.js
+++ 자료구조, 알고리즘/클래스 2/1966.js
@@ -5,9 +5,34 @@ const t = Number(init[0]);
 // 3일 때 1,3,5
 for (let i = 1; i<= 2*t-1; i=i+2) {
   // N : 문서의 개수, M : 현재 queue에서 궁금한 문서의 위치 정보
+
   const [N,M] = init[i].split(" ").map(Number);
-  const importants = init[i+1].split(" ").map(Number);
+  const importants = init[i+1].split(" ").map(Number); 
+  const number = importants[M];
+  const number2 = number - 0.1;  
+  importants[M] = number2;
+
   
-  while(true){
-  }
+  let count = 0;
+  while(true) {
+
+    const max = Math.max(...importants);
+    if (max === number | max === number2) {
+      break;
+    }
+    // max 값을 뽑았다면 shift 시키고 count + 1 해주고 반복 종료
+    while(true) {
+      if(importants[0] === max) {
+        importants.shift();
+        count = count + 1;
+        break;
+      }
+      else {
+        importants.push(importants.shift());
+      }
+    }    
+  }  
 }
+
+
+
