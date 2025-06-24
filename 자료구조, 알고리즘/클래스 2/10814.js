3e85947cd3c62c4da4a394f4ed67232807b29852
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-25 08:50:46 +0900

    정렬

--- 자료구조, 알고리즘/클래스 2/10814.js
+++ 자료구조, 알고리즘/클래스 2/10814.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
-const init = fs.readFileSync("example.txt").toString().trim().split("\n");
+const init = fs.readFileSync("example.txt").toString().trim().split("\n").map((line) => line.split(" "));
 const n = Number(init.shift());
+const result = []
 
- init.forEach((s, index) => {
-  init[index].split(" ");
- })
+init.sort((a,b) => {
+  return (Number(a[0]) - Number(b[0])); 
+});
 
-console.log(init);
+
+console.log(init.map(arr => arr.join(" ")).join("\n"));
+// console.log(init.join("\n"));
