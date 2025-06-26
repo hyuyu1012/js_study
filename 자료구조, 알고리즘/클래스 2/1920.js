ae1f0e2b017a8d567edbb664b628b58586085214
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-26 12:50:57 +0900

    재귀함수에 맞게 return 문 수정

--- 자료구조, 알고리즘/클래스 2/1920.js
+++ 자료구조, 알고리즘/클래스 2/1920.js
@@ -2,8 +2,15 @@ const fs = require('fs');
 const init = fs.readFileSync("example.txt").toString().trim().split("\n");
 
 const arr = init[1].split(" ").map(Number).sort((a,b) => a-b);
-const needFindNumber = init[3].split(" ").map(Number);
+const findNumbers = init[3].split(" ").map(Number);
 
-needFindNumber.forEach(number => {
+function binarySearch(arr, number, low, mid, high) {
   
+}
+
+
+
+
+findNumbers.forEach((number) => {
+
 })
