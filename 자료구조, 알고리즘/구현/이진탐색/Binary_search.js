a0ecd234357d9a429690fecd9b41d1089521fb56
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-26 10:43:38 +0900

    환경 세팅

--- 자료구조, 알고리즘/구현/이진탐색/Binary_search.js
+++ 자료구조, 알고리즘/구현/이진탐색/Binary_search.js
@@ -1,3 +1,16 @@
-// 테스트용
+// 테스트용 배열
 const arr = [1,2,3,4,5];
 const arr2 = [1,2,3,4];
+const number = 1;
+
+// 함수 선언
+function binarySearch(arr, low, mid, high, number) {
+  
+}
+
+
+// 함수 실행
+binarySearch(arr, 0, Math.trunc((0 + arr.length) /2), arr.length, number);
+// console.log(Math.trunc((0 + arr.length) /2));
+
+
