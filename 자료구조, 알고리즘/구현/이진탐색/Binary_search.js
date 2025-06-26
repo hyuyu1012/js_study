4f87250e4add2491080b933871b18a5509b8ca94
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-26 11:11:06 +0900

    숫자 없는 부분 제외하고 구현 완료

--- 자료구조, 알고리즘/구현/이진탐색/Binary_search.js
+++ 자료구조, 알고리즘/구현/이진탐색/Binary_search.js
@@ -1,16 +1,30 @@
 // 테스트용 배열
-const arr = [1,2,3,4,5];
+const arr = [1,2,3,4,5,6,7,8,9,10];
 const arr2 = [1,2,3,4];
-const number = 1;
+const number = 8;
 
-// 함수 선언
+// 함수 선언(재귀 함수)
 function binarySearch(arr, low, mid, high, number) {
-  
+  console.log(`mid : ${mid}....`)
+  if(arr[mid] === number) {
+    console.log(`숫자를 찾았습니다. 숫자가 존재하는 인덱스${mid}`);
+    return mid;
+  }
+  else if (arr[mid] > number ) {
+    high = mid;
+    mid = Math.trunc((low + high) / 2);
+
+    binarySearch(arr, low, mid, high, number)
+  }
+
+  else if (arr[mid] < number) {
+    low =  mid;
+    mid = Math.trunc((low + high) / 2);
+    binarySearch(arr, low, mid, high, number)  
+  }
+
 }
 
-
-// 함수 실행
 binarySearch(arr, 0, Math.trunc((0 + arr.length) /2), arr.length, number);
-// console.log(Math.trunc((0 + arr.length) /2));
 
 
