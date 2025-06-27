730a2b1b27cc5d36d9da35b0e65d33bde5d81069
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-27 11:30:49 +0900

    수학(에라토스테네스의 체)

--- 자료구조, 알고리즘/클래스 2/1929.js
+++ 자료구조, 알고리즘/클래스 2/1929.js
@@ -1,3 +1,17 @@
-const fs = require('fs');
-const [N,M] = fs.readFileSync("example.txt").toString().trim().split(" ").map(Number);
+// 소수 구하기
+// 에라토스테네스의 체
 
+const fs = require('fs');
+const [M,N] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let arr = Array.from(new Array(N-M+1), (x, i) => i + M).filter((number) => number !== 1);
+const end = Math.sqrt(N).toFixed(0);
+
+for (let i = 2; i<=end; i++) {
+   arr.forEach((number, index) => {
+    if(number % i === 0 && number !== i) {
+      delete arr[index];
+    }
+   })
+}
+arr = arr.filter((element) => element !== undefined);
+console.log(arr.join("\n"));
