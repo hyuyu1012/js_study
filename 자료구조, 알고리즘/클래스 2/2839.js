d5c423a43282714c339c2f76e536f86aa5080240
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-28 13:02:26 +0900

    2839

--- 자료구조, 알고리즘/클래스 2/2839.js
+++ 자료구조, 알고리즘/클래스 2/2839.js
@@ -1,11 +1,27 @@
 const fs = require('fs');
 const n = parseInt(fs.readFileSync("example.txt").toString().trim());
+const result = [];
 
-let result = 0;
-let result2 = 0;
-let result3 = 0;
-
-if (n % 5 === 0) {
-  result = n /5;
+if (n % 3 === 0) {
+  if(n % 5 === 0) {
+    result.push(n/5);
+  }
+  else{
+    result.push(n/3);
+  }
 }
 
+if((n % 5) % 3 === 0) {
+  result.push(Math.trunc(n/5) + (n % 5)/3);
+}
+
+if((n % 3) % 5 === 0){
+   result.push(Math.trunc(n/3) + (n % 3)/5);
+ }
+
+if (result.length === 0 ) {
+  console.log(-1);
+}
+else {
+  console.log(Math.min(...result));
+}
