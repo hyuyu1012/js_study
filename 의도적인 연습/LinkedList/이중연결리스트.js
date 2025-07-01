f9f64f81cd605e901e02c437d3407020e9bf7811
hyuyu1012 <hanhyeongu495@gmail.com>
2025-07-01 18:25:54 +0900

    첫번째 위치에 삽입

--- 의도적인 연습/LinkedList/이중연결리스트.js
+++ 의도적인 연습/LinkedList/이중연결리스트.js
@@ -20,11 +20,13 @@ class DoubleLinkedList {
     }
     else {
       newNode.next = this.head
+      this.head.prev = newNode;
       this.head = newNode;
       this.length++;
     }
   }
 
+  // 중간 삽입 연산
   insertMiddleNode(data,index) {
     if(index < 0 || index > this.length) {
       console.log("잘못된 인덱스 범위입니다.");
@@ -36,22 +38,26 @@ class DoubleLinkedList {
     }
     
     let count = 0;
-    let preNode = this.head;
+    let currentNode = this.head;
     let nextNode = this.head.next;
 
-    while(count < index) {
+    const newNode = new Node(data);
+    //preNode를 찾아서 연산을 한다.
+    while(count < index - 1) {
+      currentNode = currentNode.next;
+      nextNode = currentNode.next;
       count++;
     }
-    
+    currentNode.next = newNode;
+    newNode.prev = currentNode;
+    newNode.next = nextNode;
+    nextNode.prev = newNode;
   }
+
 }
-
-
-
-
-
 
 const test = new DoubleLinkedList();
 test.insertFirstNode(1);
 test.insertFirstNode(2);
+// test.insertMiddleNode(3,2)
 console.log(test);
