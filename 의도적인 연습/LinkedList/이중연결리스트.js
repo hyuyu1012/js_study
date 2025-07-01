489278d4cc8b04d09a23dcd46555fee0e4f14df0
hyuyu1012 <hanhyeongu495@gmail.com>
2025-07-01 15:38:58 +0900

    삽입미완

--- 의도적인 연습/LinkedList/이중연결리스트.js
+++ 의도적인 연습/LinkedList/이중연결리스트.js
@@ -1,3 +1,4 @@
+// js에서는 this를 생략할 수 없다.
 class Node {
   constructor(data) {
     this.data = data;
@@ -5,7 +6,6 @@ class Node {
     this.next = null;
   }
 }
-
 class DoubleLinkedList {
   constructor() {
     this.head = null;
@@ -13,8 +13,45 @@ class DoubleLinkedList {
   }
 
   insertFirstNode(data) {
-    const newNode = Node(data, null, null);
-    this.head = newNode;
-    this.lneght++;
+    const newNode = new Node(data, null, null);
+    if(this.length === 0) {
+      this.head = newNode;
+      this.length++;
+    }
+    else {
+      newNode.next = this.head
+      this.head = newNode;
+      this.length++;
+    }
+  }
+
+  insertMiddleNode(data,index) {
+    if(index < 0 || index > this.length) {
+      console.log("잘못된 인덱스 범위입니다.");
+      return;
+    }
+
+    if(index === 0) {
+      return this.insertFirstNode();
+    }
+    
+    let count = 0;
+    let preNode = this.head;
+    let nextNode = this.head.next;
+
+    while(count < index) {
+      count++;
+    }
+    
   }
 }
+
+
+
+
+
+
+const test = new DoubleLinkedList();
+test.insertFirstNode(1);
+test.insertFirstNode(2);
+console.log(test);
