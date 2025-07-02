74d0636a24e67d90c3f654e68e99832ee0e50dcd
hyuyu1012 <hanhyeongu495@gmail.com>
2025-07-02 10:37:06 +0900

    검색node

--- 자료구조, 알고리즘/클래스 2/2164.js
+++ 자료구조, 알고리즘/클래스 2/2164.js
@@ -1,3 +1,43 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync("example.txt").toString().trim());
 
+class Node{
+  constructor(data){
+    this.data = data;
+    this.prev = null;
+    this.next = null;
+  }
+}
+class DoubleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  insertFirst(data) {
+    const newNode = new Node(data);
+
+    if(this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;  
+    }
+    else{
+    newNode.next = this.head
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;    
+    }
+  }
+}
+
+const list = new DoubleLinkedList();
+list.insertFirst(1);
+list.insertFirst(2);
+
+console.log(list);
+
+
+
+// while(true) {}
