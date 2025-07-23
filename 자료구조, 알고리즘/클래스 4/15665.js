94da45beb62a7f0a39c602ce4d19797dfcf4c21b
hyuyu1012 <hanhyeongu495@gmail.com>
2025-07-23 21:30:46 +0900

    7/23

---
+++ 자료구조, 알고리즘/클래스 4/15665.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const array = input[1].split(" ").map(Number).sort((a,b) => a-b);
+const result = [];
+
+const dfs = (n, arr) => {
+  if(n === M) {
+    const temp = arr.join(" ");
+    return;
+  }
+
+  for(let i = 0; i <= array.length - 1; i++) {
+    arr.push((array[i]));
+    dfs(n+1, arr);
+    arr.pop();
+  }
+}
+
+dfs(0, []);
+console.log(result.join("\n"));
