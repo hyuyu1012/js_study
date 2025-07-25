51e6e7f3a51d35ca5af6709057e8b1bc5745e2c6
hyuyu1012 <hanhyeongu495@gmail.com>
2025-07-26 07:47:45 +0900

    7/26

--- 자료구조, 알고리즘/클래스 3/17626.js
+++ 자료구조, 알고리즘/클래스 3/17626.js
@@ -2,12 +2,16 @@ const fs = require('fs');
 const n = Number(fs.readFileSync("example.txt").toString().trim());
 
 const dp = [];
-const
 
 dp[0] = 0;
 dp[1] = 1;
-
+let count = 1;
 for(let i = 2; i <=n; i++) {
-  if()
-  dp[i] =
+  if(i >= (count + 1) * (count + 1)) {
+    count = count + 1;
+  }
+  dp[i] = 1 + dp[i- count **2];
 }
+
+console.log(dp);
+console.log(dp[n]);
