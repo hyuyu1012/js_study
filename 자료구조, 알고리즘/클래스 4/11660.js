78afddb9811813a4445abf5b05987cdefed73a07
hyuyu1012 <hanhyeongu495@gmail.com>
2025-08-01 17:05:17 +0900

    8월 1일

---
+++ 자료구조, 알고리즘/클래스 4/11660.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+
+for(let i  = 1; i <=N; i++) {
+  
+}
--- 자료구조, 알고리즘/클래스 4/9251.js
+++ 자료구조, 알고리즘/클래스 4/9251.js
@@ -1,2 +1,6 @@
 const fs = require('fs');
 const [str1, str2] = fs.readFileSync("example.txt").toString().split("\n");
+
+for(let i = 0; i < str2.length; i++) {
+  console.log(str2[i]);
+}
